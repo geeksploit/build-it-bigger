323621e5084358d4008c445cf6712377e8fb9b1e
Max Rem <max@geeksploit.me>
2018-08-06 17:20:11 +0300

    feat: Add IdleResource test boilerplate

--- app/src/androidTest/java/me/geeksploit/builditbigger/JokeLoadingTest.java
+++ app/src/androidTest/java/me/geeksploit/builditbigger/JokeLoadingTest.java
@@ -1,7 +1,13 @@
 package me.geeksploit.builditbigger;
 
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -13,8 +19,28 @@ import static org.junit.Assert.assertTrue;
 @RunWith(AndroidJUnit4.class)
 public class JokeLoadingTest {
 
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+    private IdlingResource mIdlingResource;
+
+    @Before
+    public void registerIdlingResource() {
+        mIdlingResource = mActivityRule.getActivity().getIdlingResource();
+        // To prove that the test fails, omit this call:
+        Espresso.registerIdlingResources(mIdlingResource);
+    }
+
     @Test
     public void loadJoke() {
         assertTrue(true);
+    }
+
+    @After
+    public void unregisterIdlingResource() {
+        if (mIdlingResource != null) {
+            Espresso.unregisterIdlingResources(mIdlingResource);
+        }
     }
 }
