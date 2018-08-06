b3a5c551a24ac976013dcb9a5858ff470376d68d
Max Rem <max@geeksploit.me>
2018-08-06 21:54:22 +0300

    feat: Remove ads from paid flavor

--- app/src/paid/java/me/geeksploit/builditbigger/MainActivity.java
+++ app/src/paid/java/me/geeksploit/builditbigger/MainActivity.java
@@ -1,9 +1,5 @@
 package me.geeksploit.builditbigger;
 
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.InterstitialAd;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -23,13 +19,11 @@ import me.geeksploit.androidjokes.JokeActivity;
 public class MainActivity extends AppCompatActivity
     implements  EndpointsAsyncTask.DoneCallback {
     // Remove the below line after defining your own ad unit ID.
-    private static final String TOAST_TEXT = "Test ads are being shown. "
-            + "To show live ads, replace the ad unit ID in res/values/strings.xml with your own ad unit ID.";
+    private static final String TOAST_TEXT = "There are no ads being shown.";
 
     private static final int START_LEVEL = 1;
     private int mLevel;
     private Button mNextLevelButton;
-    private InterstitialAd mInterstitialAd;
     private TextView mLevelTextView;
 
     // The Idling Resource which will be null in production.
@@ -52,9 +46,6 @@ public class MainActivity extends AppCompatActivity
         // Create the text view to show the level number.
         mLevelTextView = (TextView) findViewById(R.id.level);
         mLevel = START_LEVEL;
-
-        // Create the InterstitialAd and set the adUnitId (defined in values/strings.xml).
-        mInterstitialAd = newInterstitialAd();
 
         // Toasts the test ad message on the screen. Remove this after defining your own ad unit ID.
         Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_LONG).show();
@@ -83,6 +74,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     private void showJoke() {
+        ((Button) findViewById(R.id.next_level_button)).setEnabled(false);
         if (mIdlingResource != null) mIdlingResource.setIdleState(false);
         new EndpointsAsyncTask().execute(this);
     }
@@ -91,55 +83,13 @@ public class MainActivity extends AppCompatActivity
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         // TODO: is there a better way to trigger an ad once we come back from JokeActivity?
-        loadInterstitial();
-    }
-
-    private InterstitialAd newInterstitialAd() {
-        InterstitialAd interstitialAd = new InterstitialAd(this);
-        interstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
-        interstitialAd.setAdListener(new AdListener() {
-            @Override
-            public void onAdLoaded() {
-                mNextLevelButton.setEnabled(true);
-                showInterstitial();
-            }
-
-            @Override
-            public void onAdFailedToLoad(int errorCode) {
-                mNextLevelButton.setEnabled(true);
-            }
-
-            @Override
-            public void onAdClosed() {
-                // Proceed to the next level.
-                goToNextLevel();
-            }
-        });
-        return interstitialAd;
-    }
-
-    private void showInterstitial() {
-        // Show the ad if it's ready. Otherwise toast and reload the ad.
-        if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
-            mInterstitialAd.show();
-        } else {
-            Toast.makeText(this, "Ad did not load", Toast.LENGTH_SHORT).show();
-            goToNextLevel();
-        }
-    }
-
-    private void loadInterstitial() {
-        // Disable the next level button and load the ad.
-        mNextLevelButton.setEnabled(false);
-        AdRequest adRequest = new AdRequest.Builder()
-                .setRequestAgent("android_studio:ad_template").build();
-        mInterstitialAd.loadAd(adRequest);
+        goToNextLevel();
+        ((Button) findViewById(R.id.next_level_button)).setEnabled(true);
     }
 
     private void goToNextLevel() {
-        // Show the next level and reload the ad to prepare for the level after.
+        // Show the next level.
         mLevelTextView.setText("Level " + (++mLevel));
-        mInterstitialAd = newInterstitialAd();
     }
 
     /**
