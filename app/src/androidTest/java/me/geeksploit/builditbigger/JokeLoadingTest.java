7ff52669edf5859d00a20e9702464185e54b9b66
Max Rem <max@geeksploit.me>
2018-08-06 03:22:07 +0300

    feat: Add loading joke test stub

---
+++ app/src/androidTest/java/me/geeksploit/builditbigger/JokeLoadingTest.java
@@ -0,0 +1,20 @@
+package me.geeksploit.builditbigger;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Instrumented test, which will verify that the AsyncTask is indeed loading jokes.
+ */
+@RunWith(AndroidJUnit4.class)
+public class JokeLoadingTest {
+
+    @Test
+    public void loadJoke() {
+        assertTrue(true);
+    }
+}
